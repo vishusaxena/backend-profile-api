30d51b9d11c336bf36ad2afad1f2fa1082c23247
vishusaxena <vishusaxena3105@gmail.com>
2025-03-30 12:16:45 +0530

    Added User model schema

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    address: { type: String, required: true },
+    bio: { type: String },
+    profilePicture: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
